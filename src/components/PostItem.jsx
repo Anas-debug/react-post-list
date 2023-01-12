dbd1f5b15274e914d9dee4cb7d141aab2a777b84
Anas Saoui <saouianas@hotmail.fr>
2023-01-12 18:38:00 +0100

    Fetching data and rendering them into cards works now

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -3,8 +3,8 @@ import Card from "react-bootstrap/Card";
 
 function BasicExample({ post }) {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+    // <Card style={{ width: "18rem" }}>
+    <Card>
       <Card.Body>
         <Card.Title>{post.title}</Card.Title>
         <Card.Text>{post.body}</Card.Text>
--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -3,20 +3,23 @@ import { useState, useEffect } from "react";
 import PostItem from "../components/PostItem";
 
 function PostList() {
-  const [postList, setPostList] = useState();
+  const [postData, setPostData] = useState([]);
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((json) => {
         console.log(json);
-        setPostList(json);
+        setPostData(json);
       });
   }, []);
 
   // return <pre>{JSON.stringify(postList, undefined, 2)}</pre>;
   return (
     <div>
-      <PostItem />
+      {postData.map((post) => (
+        // <h1 key={post.id}>{post.title}</h1>
+        <PostItem key={post.id} post={post} />
+      ))}
     </div>
   );
 }
