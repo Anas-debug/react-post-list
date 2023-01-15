44e519ff52af29fb30d479bec996074ac2e3ee7f
Anas Saoui <saouianas@hotmail.fr>
2023-01-15 04:04:56 +0100

    Added a Navbar to the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import PostList from "./pages/PostList";
 import router from "./router";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <PostList />
       {/* <RouterProvider router={router} /> */}
     </div>
